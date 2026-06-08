bd0b52b2b55682869273d2a2cafc891acaa98f41
Arjun V V <arjunvv@carestack.com>
2026-06-08 11:22:01 +0530

    app component changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  {
+    path: 'home',
+    loadComponent: () => import('./features/home/home.component').then(m => m.HomeComponent)
+  },
+  {
+    path: 'about',
+    loadComponent: () => import('./features/about/about.component').then(m => m.AboutComponent)
+  },
+  {
+    path: 'experience',
+    loadComponent: () => import('./features/experience/experience.component').then(m => m.ExperienceComponent)
+  },
+  {
+    path: 'skills',
+    loadComponent: () => import('./features/skills/skills.component').then(m => m.SkillsComponent)
+  },
+  {
+    path: 'education',
+    loadComponent: () => import('./features/education/education.component').then(m => m.EducationComponent)
+  },
+  {
+    path: 'contact',
+    loadComponent: () => import('./features/contact/contact.component').then(m => m.ContactComponent)
+  },
+  { path: '**', redirectTo: 'home' }
+];
